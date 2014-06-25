c1f15dd4ec576ddbfb1780d89b07257057371e06
Jason Antman <jason@jasonantman.com>
2014-06-25 08:10:57 -0400

    add options to manage pg_hba_conf rule

--- manifests/database.pp
+++ manifests/database.pp
@@ -20,6 +20,16 @@
 #   password for $database_password
 #  (default $nodemeister::params::database_password)
 #
+# [*manage_pg_hba_conf*]
+#   boolean, if true add an appropriate pg_hba.conf rule for the NodeMeister database.
+#   must also specify nodemeister_ips if this is specified.
+#   (default: false)
+#
+# [*nodemeister_ips*]
+#   (string or array of strings) one or array of IP addresses / pg_hba.conf host
+#   specifiers to allow to access the NodeMeister database via pg_hba.conf.
+#   This only takes effect if manage_pg_hba_conf is true.
+#
 # === Variables:
 #
 # This module uses no global variables.
@@ -46,13 +56,28 @@ class nodemeister::database(
   $database_name             = $nodemeister::params::database_name,
   $database_username         = $nodemeister::params::database_username,
   $database_password         = $nodemeister::params::database_password,
+  $manage_pg_hba_conf        = false,
+  $nodemeister_ips           = undef,
 ) inherits nodemeister::params {
+
+  if ( $manage_pg_hba_conf == true and $nodemeister_ips == undef) {
+    fail('You must specify $nodemeister_ips if $manage_pg_hba_conf is true')
+  }
 
   # create the NodeMeister database
   postgresql::server::db{ $database_name:
     user     => $database_username,
     password => $database_password,
     grant    => 'ALL',
+  }
+
+  postgresql::server::pg_hba_rule { 'nodemeister':
+    type        => 'host',
+    database    => $database_name,
+    user        => $database_username,
+    address     => $nodemeister_ips,
+    auth_method => 'md5',
+    order       => '333',
   }
 
 }
